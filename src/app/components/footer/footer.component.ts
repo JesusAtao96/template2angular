0589b768817ffb9ee236127b067eb155b778a7c4
Jesús Alejandro Atao <jealeat96@gmail.com>
2018-01-22 18:20:41 -0500

    Header, Body y Footer listo!

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: []
+})
+export class FooterComponent {
+
+  year: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
