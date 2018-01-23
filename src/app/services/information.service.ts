509ffbb3cbff471a051e044195565fd59c8731df
Jesús Alejandro Atao <jealeat96@gmail.com>
2018-01-22 20:16:17 -0500

    Servicio de firebase listo!

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -7,11 +7,28 @@ export class InformationService {
   info: any = {};
   load: boolean = false;
 
+  team: any = {}
+  isLoadAbout: boolean = false;
+
   constructor(public http: Http) {
+    this.loadInfo();
+    this.loadAbout();
+  }
+
+  loadInfo() {
     this.http.get('assets/data/info.page.json')
       .subscribe(data => {
         this.load = true;
         this.info = data.json();
+      })
+  }
+
+  loadAbout() {
+    this.http.get('https://paginaweb-221ec.firebaseio.com/team.json')
+      .subscribe(data => {
+        console.log(data.json());
+        this.isLoadAbout = true;
+        this.team = data.json();
       })
   }
 
